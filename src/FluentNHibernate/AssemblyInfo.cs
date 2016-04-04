3c61f55ff815ddf8341c4ce849074beb65fcc525
Alexander T. <tarassov.al@gmail.com>
2016-04-04 19:56:17 +0300

    AssemlyInfo

---
+++ src/FluentNHibernate/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CKI FluentNHibernate")]
+[assembly: AssemblyDescription("This is a fork from https://github.com/jagregory/fluent-nhibernate/tree/master/src/FluentNHibernate")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CKI")]
+[assembly: AssemblyProduct("CKIFluentNHibernate")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8a60f332-d177-4b31-8f1a-f6b6def789a3")]
+
+
+[assembly: InternalsVisibleTo("FluentNHibernate.Testing")]
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("2.0.3.0")]
+[assembly: AssemblyFileVersion("2.0.3.0")]
